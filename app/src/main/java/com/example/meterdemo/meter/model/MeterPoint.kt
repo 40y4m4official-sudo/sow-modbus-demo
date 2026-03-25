033e7e21a522c75605c4c29e2b408b99008c5145
Hirota Aoyama <40Y4M4.official@gmail.com>
2026-03-25 16:38:41 +0900

    エラー修正

--- app/src/main/java/com/example/meterdemo/meter/model/MeterPoint.kt
+++ app/src/main/java/com/example/meterdemo/meter/model/MeterPoint.kt
@@ -6,9 +6,9 @@ data class MeterPoint(
     val registerCount: Int,
     val gain: Int,
     val dataType: DataType,
-    val wordByteOrder: WordByteOrder = WordByteOrder.MSB_MSB,
     val unit: String = "",
-    val initialRawValue: Int
+    val initialRawValue: Int,
+    val wordByteOrder: WordByteOrder = WordByteOrder.MSB_MSB
 ) {
     fun formattedValue(rawValue: Int): String {
         return if (gain <= 1) {
