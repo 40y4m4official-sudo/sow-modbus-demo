dac6f7feb04764e8085f57190425d4def0f502e9
Hirota Aoyama <40Y4M4.official@gmail.com>
2026-03-25 21:23:07 +0900

    BackUP-CT規定値

--- app/src/main/java/com/example/meterdemo/meter/profile/BackUpCtProfile.kt
+++ app/src/main/java/com/example/meterdemo/meter/profile/BackUpCtProfile.kt
@@ -15,21 +15,21 @@ object BackUpCtProfile {
         stopBits = 1,
         functionCode = 0x03,
         points = listOf(
-            MeterPoint("A相電圧", 37101, 2, 10.0, DataType.INT32, "V", 0),
-            MeterPoint("B相電圧", 37103, 2, 10.0, DataType.INT32, "V", 0),
-            MeterPoint("A相電流", 37107, 2, 100.0, DataType.INT32, "A", 0),
-            MeterPoint("B相電流", 37109, 2, 100.0, DataType.INT32, "A", 0),
-            MeterPoint("有効電力", 37113, 2, 1000.0, DataType.INT32, "kW", 0),
-            MeterPoint("無効電力", 37115, 2, 1000.0, DataType.INT32, "kVar", 0),
-            MeterPoint("力率", 37117, 1, 1000.0, DataType.INT16, "", 0),
-            MeterPoint("正方向合計有効電力量", 37119, 2, 100.0, DataType.INT32, "kWh", 0),
-            MeterPoint("負方向合計有効電力量", 37121, 2, 100.0, DataType.INT32, "kWh", 0),
-            MeterPoint("合計無効電力量", 37123, 2, 100.0, DataType.INT32, "kVarh", 0),
-            MeterPoint("A-B線電圧", 37126, 2, 10.0, DataType.INT32, "V", 0),
-            MeterPoint("B-C線電圧", 37128, 2, 10.0, DataType.INT32, "V", 0),
-            MeterPoint("C-A線電圧", 37130, 2, 10.0, DataType.INT32, "V", 0),
-            MeterPoint("A相有効電力", 37132, 2, 1000.0, DataType.INT32, "kW", 0),
-            MeterPoint("B相有効電力", 37134, 2, 1000.0, DataType.INT32, "kW", 0)
+            MeterPoint("A相電圧", 37101, 2, 10.0, DataType.INT32, "V", 1010),
+            MeterPoint("B相電圧", 37103, 2, 10.0, DataType.INT32, "V", 1010),
+            MeterPoint("A相電流", 37107, 2, 100.0, DataType.INT32, "A", 500),
+            MeterPoint("B相電流", 37109, 2, 100.0, DataType.INT32, "A", 400),
+            MeterPoint("有効電力", 37113, 2, 1000.0, DataType.INT32, "kW", 863550),
+            MeterPoint("無効電力", 37115, 2, 1000.0, DataType.INT32, "kVar", 283600),
+            MeterPoint("力率", 37117, 1, 1000.0, DataType.INT16, "", 950),
+            MeterPoint("正方向合計有効電力量", 37119, 2, 100.0, DataType.INT32, "kWh", 12345),
+            MeterPoint("負方向合計有効電力量", 37121, 2, 100.0, DataType.INT32, "kWh", 4321),
+            MeterPoint("合計無効電力量", 37123, 2, 100.0, DataType.INT32, "kVarh", 111100),
+            MeterPoint("A-B線電圧", 37126, 2, 10.0, DataType.INT32, "V", 2020),
+            MeterPoint("B-C線電圧", 37128, 2, 10.0, DataType.INT32, "V", 1010),
+            MeterPoint("C-A線電圧", 37130, 2, 10.0, DataType.INT32, "V", 1010),
+            MeterPoint("A相有効電力", 37132, 2, 1000.0, DataType.INT32, "kW", 479750),
+            MeterPoint("B相有効電力", 37134, 2, 1000.0, DataType.INT32, "kW", 383800)
         )
     )
 }
