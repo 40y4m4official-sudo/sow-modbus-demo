f01a6d3c5b3196c8087f667d8722150f11c8ae90
Hirota Aoyama <40Y4M4.official@gmail.com>
2026-03-27 16:54:46 +0900

    ビルドエラー修正

--- app/src/test/java/com/example/meterdemo/logging/LogAddressSummaryAnalyzerTest.kt
+++ app/src/test/java/com/example/meterdemo/logging/LogAddressSummaryAnalyzerTest.kt
@@ -1,7 +1,7 @@
 package com.example.meterdemo.logging
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class LogAddressSummaryAnalyzerTest {
 
