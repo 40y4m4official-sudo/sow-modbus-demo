d6e6b650c5bf2d0decbf3d4df04c4b81ee3860f5
Hirota Aoyama <40Y4M4.official@gmail.com>
2026-03-28 13:16:13 +0900

    SSR-MB初期値巻き戻し

--- app/src/main/java/com/example/meterdemo/meter/profile/MitsubishiMe110SsrMbProfile.kt
+++ app/src/main/java/com/example/meterdemo/meter/profile/MitsubishiMe110SsrMbProfile.kt
@@ -25,12 +25,12 @@ object MitsubishiMe110SsrMbProfile {
             MeterPoint("A相電流", 768, 1, 1.0, DataType.INT, "A", 1, signalType = SignalType.PHASE_A_CURRENT),
             MeterPoint("B相電流", 769, 1, 1.0, DataType.INT, "A", 2, signalType = SignalType.PHASE_B_CURRENT),
             MeterPoint("C相電流", 770, 1, 1.0, DataType.INT, "A", 3, signalType = SignalType.PHASE_C_CURRENT),
-            MeterPoint("有効電力", 794, 1, 10000.0, DataType.INT, "kW", 380000, signalType = SignalType.ACTIVE_POWER_TOTAL),
-            MeterPoint("A相有効電力", 791, 1, 10000.0, DataType.INT, "kW", 60000, signalType = SignalType.PHASE_A_ACTIVE_POWER),
-            MeterPoint("B相有効電力", 792, 1, 10000.0, DataType.INT, "kW", 130000, signalType = SignalType.PHASE_B_ACTIVE_POWER),
-            MeterPoint("C相有効電力", 793, 1, 10000.0, DataType.INT, "kW", 190000, signalType = SignalType.PHASE_C_ACTIVE_POWER),
-            MeterPoint("無効電力", 802, 1, 10000.0, DataType.INT, "kVar", 120000, signalType = SignalType.REACTIVE_POWER_TOTAL),
-            MeterPoint("皮相電力", 806, 1, 10000.0, DataType.INT, "kVA", 40000, signalType = SignalType.APPARENT_POWER_TOTAL),
+            MeterPoint("有効電力", 794, 1, 10000.0, DataType.INT, "kW", 376200, signalType = SignalType.ACTIVE_POWER_TOTAL),
+            MeterPoint("A相有効電力", 791, 1, 10000.0, DataType.INT, "kW", 62700, signalType = SignalType.PHASE_A_ACTIVE_POWER),
+            MeterPoint("B相有効電力", 792, 1, 10000.0, DataType.INT, "kW", 125400, signalType = SignalType.PHASE_B_ACTIVE_POWER),
+            MeterPoint("C相有効電力", 793, 1, 10000.0, DataType.INT, "kW", 188100, signalType = SignalType.PHASE_C_ACTIVE_POWER),
+            MeterPoint("無効電力", 802, 1, 10000.0, DataType.INT, "kVar", 123500, signalType = SignalType.REACTIVE_POWER_TOTAL),
+            MeterPoint("皮相電力", 806, 1, 10000.0, DataType.INT, "kVA", 39600, signalType = SignalType.APPARENT_POWER_TOTAL),
             MeterPoint("正方向合計有効電力量", 1304, 2, 1.0, DataType.INT, "kWh", 1235, signalType = SignalType.FORWARD_ACTIVE_ENERGY_TOTAL),
             MeterPoint("負方向合計有効電力量", 1306, 2, 1.0, DataType.INT, "kWh", 1, signalType = SignalType.REVERSE_ACTIVE_ENERGY_TOTAL)
         )
