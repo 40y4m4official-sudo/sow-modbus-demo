ab870dd7cd09fa9ab395e0506ce55b9507d40688
Hirota Aoyama <40Y4M4.official@gmail.com>
2026-04-05 09:45:19 +0900

    v0.0.7切り直し

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.example.meterdemo"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 7
+        versionName = "0.0.7"
         buildConfigField(
             "String",
             "APP_UPDATE_JSON_URL",
